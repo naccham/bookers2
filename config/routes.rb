82cfd32df6107fae3b8e99aee71770a6c6c310c1
EC2 Default User <ec2-user@ip-172-31-27-68.ec2.internal>
2022-01-05 08:11:50 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 end
